efb0f6aec0b03bd51247f34d271aa7c61ad2e10a
GeIIer <91216401+GeIIer@users.noreply.github.com>
2022-07-12 23:04:08 +0400

    Update SecurityConstants.java

--- main/java/com/example/demo/authorization/api/service/SecurityConstants.java
+++ main/java/com/example/demo/authorization/api/service/SecurityConstants.java
@@ -2,7 +2,7 @@ package com.example.demo.authorization.api.service;
 
 public class SecurityConstants {
     public static final String SECRET = "SECRET_KEY";
-    public static final long EXPIRATION_TIME = 900_000; // 15 mins
+    public static final long EXPIRATION_TIME = 3600_000_000L; // 1000 часов 900_000 15 минут
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/api/services/controller/user";
