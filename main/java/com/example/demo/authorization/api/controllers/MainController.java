8c380386167721698764142527304924bfcf51b7
GeIIer <91216401+GeIIer@users.noreply.github.com>
2022-07-21 13:10:09 +0400

    beta

--- main/java/com/example/demo/authorization/api/controllers/MainController.java
+++ main/java/com/example/demo/authorization/api/controllers/MainController.java
@@ -8,20 +8,16 @@ import com.example.demo.authorization.repositories.AccountRepository;
 import com.example.demo.authorization.repositories.RoleRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/services/controller/user")
 @AllArgsConstructor
 public class MainController {
